7720c42dcea87e57a8c24289936e832ba4f07dac
Scott Bouzane <scottbouzane@Scotts-MacBook-Pro.local>
2021-11-12 13:49:30 -0500

    axios api

--- axios.js
+++ axios.js
@@ -1,21 +1,41 @@
-let button = document.querySelector("#searchButton")
+const DOMAIN = `https://api.mozambiquehe.re/bridge?version=5&platform=PS4&player=${textInput}&auth=${API_KEY}`;
+const API_KEY = "JYf8OElOW4fh8NLAImbE"
+const BASE_URL = '${DOMAIN}?apikey=${API_KEY}&s=';
 
-async function getData(event) {
-  event.preventDefault
-  let textInput = document.querySelector("#inputBar").value
-  const url = `https://api.mozambiquehe.re/bridge?version=5&platform=PS4&player=${textInput}&auth=JYf8OElOW4fh8NLAImbE`
+const button = document.querySelector("#searchButton");
 
-  fetch(url)
-    .then(res => {
-      return res.json();
-    })
-    .then(res => {
-      console.log("success")
-      const apexHeading = document.querySelector('#apexHeader')
-      apexHeading.src = `Leaderboards: ${res.arena}`
-      return arena;
+const getData = () => {
+  axios.get(`https://api.mozambiquehe.re/bridge?version=5&platform=PS4&player=${textInput}&auth=${API_KEY}`).then(response => {
+    console.log(response);
+  });
+};
 
-    })
-}
+const sendData = () => { };
 
 button.addEventListener("click", getData)
+
+
+
+
+
+  // / let button = document.querySelector("#searchButton")
+
+// async function getData(event) {
+//   event.preventDefault
+//   let textInput = document.querySelector("#inputBar").value
+//   const url = `https://api.mozambiquehe.re/bridge?version=5&platform=PS4&player=${textInput}&auth=${API_KEY}`
+
+//   fetch(url)
+//     .then(res => {
+//       return res.json();
+//     })
+//     .then(res => {
+//       console.log("success")
+//       const apexHeading = document.querySelector('#apexHeader')
+//       apexHeading.src = `Leaderboards: ${res.arena}`
+//       return arena;
+
+//     })
+// }
+
+// button.addEventListener("click", getData)
